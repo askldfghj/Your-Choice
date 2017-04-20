d7299d0597a425bfa009438362d027cba48e8425
askldfghj <askldfghj@naver.com>
2017-04-20 14:32:25 +0900

    인벤토리 기본 레이아웃

---
+++ YourChoice/Assets/Scripts/Inventory/CursorCtrl.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorCtrl : MonoBehaviour
+{
+    public UISprite _sprite;
+    
+    
+}
---
+++ YourChoice/Assets/Scripts/Inventory/ItemIconCtrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemIconCtrl : MonoBehaviour
+{
+    public UISprite _sprite;
+    string _spriteName = "Square";
+
+    bool _isDrag = false;
+    void SetItem(string sprName)
+    {
+        _sprite.spriteName = "Square";
+    }
+
+    void OnDrag(Vector2 delta)
+    {
+        if (_isDrag)
+        {
+
+        }
+        else
+        {
+            _isDrag = true;
+            Debug.Log("call");
+            _sprite.enabled = true;
+            _sprite.spriteName = _spriteName;
+        }
+    }
+}
